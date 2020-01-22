294b4bc0fada3a7d6ebedac62d4ee3fd0179031a
Shorts1999 <53516987+Shorts1999@users.noreply.github.com>
2020-01-22 13:09:17 +0100

    Updates.

--- main.ts
+++ main.ts
@@ -8,11 +8,15 @@
  * functions for the balanceCar:
  */
 //%weight=5 color=#80AF47 icon="\uf110"
-//% groups=["MotorControl"]
+//% groups=["MotorControl", "Ultrasonic Sensor"]
 namespace BalanceCar {
+    /**
+     * Blocks for controlling the motors:
+     */
+
     //% block="turn Left" weight=2
     //% group="MotorControl"
-    export function turnLeft() {
+    export function turnLeft(): void {
         pins.digitalWritePin(DigitalPin.P13, 1)
         pins.digitalWritePin(DigitalPin.P14, 0)
         pins.digitalWritePin(DigitalPin.P15, 1)
@@ -20,7 +24,7 @@ namespace BalanceCar {
     }
     //% block="turn Right" weight=3
     //% group="MotorControl"
-    export function turnRight() {
+    export function turnRight(): void {
         pins.digitalWritePin(DigitalPin.P13, 0)
         pins.digitalWritePin(DigitalPin.P14, 1)
         pins.digitalWritePin(DigitalPin.P15, 0)
@@ -28,7 +32,7 @@ namespace BalanceCar {
     }
     //% block="forwards" weight=0
     //% group="MotorControl"
-    export function forwards() {
+    export function forwards(): void {
         pins.digitalWritePin(DigitalPin.P13, 0)
         pins.digitalWritePin(DigitalPin.P14, 1)
         pins.digitalWritePin(DigitalPin.P15, 1)
@@ -36,10 +40,28 @@ namespace BalanceCar {
     }
     //% block="backwards" weight=1
     //% group="MotorControl"
-    export function backwards() {
+    export function backwards(): void {
         pins.digitalWritePin(DigitalPin.P13, 1)
         pins.digitalWritePin(DigitalPin.P14, 0)
         pins.digitalWritePin(DigitalPin.P15, 0)
         pins.digitalWritePin(DigitalPin.P16, 1)
+    }
+
+
+    /**
+     * Blocks for controlling the ultrasonic Sensor:
+     */
+    //% block="read distance in cm:"
+    //% group="Ultrasonic sensor" weight=1
+    export function readDistance(): number {
+        pins.digitalWritePin(DigitalPin.P8, 0)
+        control.waitMicros(2)
+        pins.digitalWritePin(DigitalPin.P8, 1)
+        control.waitMicros(10)
+        pins.digitalWritePin(DigitalPin.P8, 0)
+
+        let distance = pins.pulseIn(DigitalPin.P9, PulseValue.High)
+
+        return (distance / 58)
     }
 }
