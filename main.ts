1d7443f2cc9c00cac40db3d098c615e7a4ee359a
Shorts1999 <53516987+Shorts1999@users.noreply.github.com>
2020-09-16 13:48:44 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -4,6 +4,9 @@
  * Date: January 22nd 2020
  */
 
+/**
+ * Colours returned from the "readColour" function
+ */
 enum SensorColors {
     //%block=black
     black = "black",
@@ -123,7 +126,7 @@ namespace BalanceCar {
      * returns the RGB value of the colour under the sensor in 24-bit color mode
      * compatible to export to neopixels
      */
-    //% block="read RGB-value"
+    //% block="read RGB-value using %samples samples"
     //% group="Colour Sensor"
     // @param samples the amount of samples to take
     export function readRGB(samples:number=10): number {
@@ -132,7 +135,7 @@ namespace BalanceCar {
         let readAverage: number
         let readTotal:number=0
 
-        //Set colour mode to Red:
+        //Set colour mode to RED
         pins.digitalWritePin(DigitalPin.P0, 0)
         pins.digitalWritePin(DigitalPin.P1, 0)
         basic.pause(5)
@@ -140,24 +143,19 @@ namespace BalanceCar {
             readValue = pins.pulseIn(DigitalPin.P2, PulseValue.High)
             readTotal+=readValue
             control.waitMicros(5)
-            serial.writeNumber(i)
-            serial.writeValue(" RED ", readValue)
         }
         //divide the total number by the amount of samples to get an average.
         readAverage=(readTotal/samples) 
-        //serial.writeString("Red: ")
-        //serial.writeNumber(frequency)
         readAverage = Math.constrain(readAverage, 20,80)
         //map the read value (in microseconds) to an 8-bit number and shift it to the leftmost position in a 24-bit number.
         output |= Math.map(readAverage, 20, 80, 255, 0) << 16
-        serial.writeValue("RED TOTAL: ",  Math.map(readAverage, 20, 80, 255, 0))
         basic.pause(2)
 
         //Reset the read value
         readValue=0
         readAverage=0
         readTotal=0
-        //Set Colour mode to Green
+        //Set Colour mode to GREEN
         pins.digitalWritePin(DigitalPin.P0, 1)
         pins.digitalWritePin(DigitalPin.P1, 1)
         basic.pause(5)
@@ -165,20 +163,15 @@ namespace BalanceCar {
             readValue = pins.pulseIn(DigitalPin.P2, PulseValue.High)
             readTotal+=readValue
             control.waitMicros(5)
-            serial.writeNumber(i)
-            serial.writeValue(" Green ", readValue)
         }
         //map the read value (in microseconds) to an 8-bit number and shift it to the middle position in a 24-bit number.
         readAverage=(readTotal/samples) 
-        //serial.writeString(" Green: ")
-        //serial.writeNumber(frequency)
         readAverage = Math.constrain(readAverage, 30, 90)
         output |= Math.map(readAverage, 30, 90, 255, 0) << 8
-        serial.writeValue("GREEN TOTAL: ", Math.map(readAverage, 30, 90, 255, 0))
         basic.pause(2)
 
-        //Set Colour mode to blue
-                readValue=0
+        //Set Colour mode to BLUE
+        readValue=0
         readAverage=0
         readTotal=0
         pins.digitalWritePin(DigitalPin.P0, 1)
@@ -188,16 +181,11 @@ namespace BalanceCar {
             readValue = pins.pulseIn(DigitalPin.P2, PulseValue.High)
             readTotal+=readValue
             control.waitMicros(5)
-            serial.writeNumber(i)
-            serial.writeValue(" BLUE ", readValue)
         }
         readAverage=(readTotal/samples) 
-        //serial.writeString(" Blue: ")
-        //serial.writeNumber(frequency)
         readAverage = Math.constrain(readAverage, 20, 80)
         //map the read value (in microseconds) to an 8-bit number
         output |= Math.map(readAverage, 20, 80, 255, 0)
-        serial.writeValue("BLUE TOTAL", Math.map(readAverage, 20, 80, 255, 0))
         basic.pause(2)
 
         return output
@@ -207,74 +195,69 @@ namespace BalanceCar {
      */
     //% block="Read colour"
     //% group="Colour Sensor" weigth=10
-    export function readColour(): string {
-        let colour: string
-        let output: number = 0
-        let frequency: number = 0
-        let RED: boolean = false
-        let GREEN: boolean = false
-        let BLUE: boolean = false
+    export function readColour(samples:number=10): string {
+        let RED:boolean=false
+        let BLUE:boolean=false
+        let GREEN:boolean=false
+
+        let output: number
+        let readValue:number=0
+        let readAverage: number
+        let readTotal:number=0
+
+        //Set colour mode to RED
         pins.digitalWritePin(DigitalPin.P0, 0)
         pins.digitalWritePin(DigitalPin.P1, 0)
-        basic.pause(2)
-        //To reduce colour-read-errors, 5 measurements are taken and averaged.
-        let frequency1 = pins.pulseIn(DigitalPin.P2, PulseValue.High)
-        control.waitMicros(4)
-        let frequency2 = pins.pulseIn(DigitalPin.P2, PulseValue.High)
-        control.waitMicros(4)
-        let frequency3 = pins.pulseIn(DigitalPin.P2, PulseValue.High)
-        control.waitMicros(4)
-        let frequency4 = pins.pulseIn(DigitalPin.P2, PulseValue.High)
-        control.waitMicros(4)
-        let frequency5 = pins.pulseIn(DigitalPin.P2, PulseValue.High)
-        control.waitMicros(4)
-        frequency = (frequency1 + frequency2 + frequency3 + frequency4 + frequency5) / 5
-        if (frequency < 50) { RED = true }
+        basic.pause(5)
+        for(let i=0; i<samples; i++){
+            readValue = pins.pulseIn(DigitalPin.P2, PulseValue.High)
+            readTotal+=readValue
+            control.waitMicros(5)
+        }
+        //divide the total number by the amount of samples to get an average.
+        readAverage=(readTotal/samples) 
+        if(readAverage>60) RED=true
         basic.pause(2)
 
+        //Reset the read value
+        readValue=0
+        readAverage=0
+        readTotal=0
+        //Set Colour mode to GREEN
         pins.digitalWritePin(DigitalPin.P0, 1)
         pins.digitalWritePin(DigitalPin.P1, 1)
-        basic.pause(2)
-        //To reduce colour-read-errors, 5 measurements are taken and averaged.
-        frequency1 = pins.pulseIn(DigitalPin.P2, PulseValue.High)
-        control.waitMicros(4)
-        frequency2 = pins.pulseIn(DigitalPin.P2, PulseValue.High)
-        control.waitMicros(4)
-        frequency3 = pins.pulseIn(DigitalPin.P2, PulseValue.High)
-        control.waitMicros(4)
-        frequency4 = pins.pulseIn(DigitalPin.P2, PulseValue.High)
-        control.waitMicros(4)
-        frequency5 = pins.pulseIn(DigitalPin.P2, PulseValue.High)
-        control.waitMicros(4)
-        frequency = (frequency1 + frequency2 + frequency3 + frequency4 + frequency5) / 5
-        if (frequency < 80) { GREEN = true }
-        basic.pause(2)
+        basic.pause(5)
+        for(let i=0; i<samples; i++){
+            readValue = pins.pulseIn(DigitalPin.P2, PulseValue.High)
+            readTotal+=readValue
+            control.waitMicros(5)
+        }
+        //map the read value (in microseconds) to an 8-bit number and shift it to the middle position in a 24-bit number.
+        readAverage=(readTotal/samples) 
+        if(readAverage>70) GREEN=true
 
+        //Set Colour mode to BLUE
+        readValue=0
+        readAverage=0
+        readTotal=0
         pins.digitalWritePin(DigitalPin.P0, 1)
         pins.digitalWritePin(DigitalPin.P1, 0)
+        basic.pause(5)
+        for(let i=0; i<samples; i++){
+            readValue = pins.pulseIn(DigitalPin.P2, PulseValue.High)
+            readTotal+=readValue
+            control.waitMicros(5)
+        }
+        readAverage=(readTotal/samples) 
+        if(readAverage>60) BLUE=true
         basic.pause(2)
-        //To reduce colour-read-errors, 5 measurements are taken and averaged.
-        frequency1 = pins.pulseIn(DigitalPin.P2, PulseValue.High)
-        control.waitMicros(4)
-        frequency2 = pins.pulseIn(DigitalPin.P2, PulseValue.High)
-        control.waitMicros(4)
-        frequency3 = pins.pulseIn(DigitalPin.P2, PulseValue.High)
-        control.waitMicros(4)
-        frequency4 = pins.pulseIn(DigitalPin.P2, PulseValue.High)
-        control.waitMicros(4)
-        frequency5 = pins.pulseIn(DigitalPin.P2, PulseValue.High)
-        control.waitMicros(4)
-        frequency = (frequency1 + frequency2 + frequency3 + frequency4 + frequency5) / 5
-        if (frequency < 70) { BLUE = true }
-        basic.pause(2)
-        if (RED && !BLUE && !GREEN) { colour = "red" }
-        if (!RED && BLUE && !GREEN) { colour = "blue" }
-        if (!RED && !BLUE && GREEN) { colour = "green" }
-        if (RED && BLUE && GREEN) { colour = "white" }
-        if (!RED && !BLUE && !GREEN) { colour = "black" }
-        if (RED && BLUE && !GREEN) { colour = "purple" }
-        if (RED && !BLUE && GREEN) { colour = "yellow" }
-        if (!RED && BLUE && GREEN) { colour = "cyan" }
-        return colour
+        if (RED && !BLUE && !GREEN) { return "red" }
+        else if (!RED && BLUE && !GREEN) {  return "blue" }
+        else if (!RED && !BLUE && GREEN) { return "green" }
+        else if (RED && BLUE && GREEN) { return "white" }
+        else if (RED && BLUE && !GREEN) { return "purple" }
+        else if (RED && !BLUE && GREEN) { return "yellow" }
+        else if (!RED && BLUE && GREEN) { return "cyan" }
+        else return "black";
     }
 }
